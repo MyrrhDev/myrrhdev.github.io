18c765d1dd1da1026146392dbdc1585c55813130
Mayra <mayra.pastor@est.fib.upc.edu>
2021-03-04 14:11:24 +0100

    txt in html

--- pose/sketch.js
+++ pose/sketch.js
@@ -83,33 +83,34 @@ function modelLoaded() {
 }
 
 function draw() {
-  push();
-  translate(video.width, 0);
-  scale(-1, 1);
-  image(video, 0, 0, video.width, video.height);
-
-  if (pose) {
-    for (let i = 0; i < skeleton.length; i++) {
-      let a = skeleton[i][0];
-      let b = skeleton[i][1];
-      strokeWeight(2);
-      stroke(0);
-
-      line(a.position.x, a.position.y, b.position.x, b.position.y);
-    }
-    for (let i = 0; i < pose.keypoints.length; i++) {
-      let x = pose.keypoints[i].position.x;
-      let y = pose.keypoints[i].position.y;
-      fill(0);
-      stroke(255);
-      ellipse(x, y, 16, 16);
-    }
-  }
-  pop();
-
-  fill(255, 0, 255);
-  noStroke();
-  textSize(512);
-  textAlign(CENTER, CENTER);
-  text(poseLabel, width / 2, height / 2);
+    document.getElementById("image-label").innerHTML = poseLabel;
+//   push();
+//   translate(video.width, 0);
+//   scale(-1, 1);
+//   image(video, 0, 0, video.width, video.height);
+// 
+//   if (pose) {
+//     for (let i = 0; i < skeleton.length; i++) {
+//       let a = skeleton[i][0];
+//       let b = skeleton[i][1];
+//       strokeWeight(2);
+//       stroke(0);
+// 
+//       line(a.position.x, a.position.y, b.position.x, b.position.y);
+//     }
+//     for (let i = 0; i < pose.keypoints.length; i++) {
+//       let x = pose.keypoints[i].position.x;
+//       let y = pose.keypoints[i].position.y;
+//       fill(0);
+//       stroke(255);
+//       ellipse(x, y, 16, 16);
+//     }
+//   }
+//   pop();
+// 
+//   fill(255, 0, 255);
+//   noStroke();
+//   textSize(512);
+//   textAlign(CENTER, CENTER);
+//   text(poseLabel, width / 2, height / 2);
 }

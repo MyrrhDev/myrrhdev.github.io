63083b855249d49db7a1b11e9d8db797e93f17a2
Mayra <mayra.pastor@est.fib.upc.edu>
2021-11-23 17:45:31 +0100

    IMA webpage init

---
+++ ima/js/main.js
@@ -0,0 +1,15 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function onMenu() {
+    document.getElementById("cssmenu").style.display = "block";
+}
+
+function offMenu() {
+    document.getElementById("cssmenu").style.display = "none";
+}
